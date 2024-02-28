41234d71ea404045d0879302e89c5efb051009ba
Sahil Savardekar <sahilsavardekar2003@gmail.com>
2024-02-28 23:09:20 +0530

    created sign up page

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Label, TextInput, Button } from 'flowbite-react';
 
 export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+  return(
+    <div className='min-h-screen mt-20'>
+    <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex-1'>
+      <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-2 bg-gradient-to-r from-blue-500 via-sky-500 to-teal-300 rounded-lg text-white'>Sahil's</span>
+            Blog
+      </Link>
+      <p className='text-sm mt-5'>
+        This is a demp project. You can sign up with your email ans password or with Google.
+      </p>
+      </div>
+
+      <div className="flex-1">
+            <form className='flex flex-col gap-4'>
+                <div>
+                  <Label value='Your username'></Label>
+                  <TextInput type='text' placeholder='Username' id='username'></TextInput>
+                </div>
+                <div>
+                  <Label value='Your email'></Label>
+                  <TextInput type='text' placeholder='name@company.com' id='email'></TextInput>
+                </div>
+                <div>
+                  <Label value='Your password'></Label>
+                  <TextInput type='text' placeholder='Password' id='password'></TextInput>
+                </div>
+                <Button gradientDueTone='BlueToGray' type='submit'>
+                    Sign Up
+                </Button>
+            </form>
+            <div className='flex gap-2 text-sm mt-5'>
+              <span>Have an account?</span>
+              <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
+            </div>
+      </div>
+  </div>
+</div>
+  );
 }
